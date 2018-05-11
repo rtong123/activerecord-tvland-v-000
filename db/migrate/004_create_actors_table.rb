2fae0ebf377fc89c83859f5e91d0455d2347f5a1
Rebecca Tong <rtong@buffalo.edu>
2018-05-11 17:14:41 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors_table.rb
+++ db/migrate/004_create_actors_table.rb
@@ -1,4 +1,4 @@
-class CreateActorsTable < ActiveRecord::Migration
+class CreateActorsTable < ActiveRecord::Migration[4.2]
     def change
       create_table :actors do |t|
         t.string :first_name
