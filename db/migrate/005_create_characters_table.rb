b3f65a0197b0d34dbee530eaeb149a2577c1f13c
Rebecca Tong <rtong@buffalo.edu>
2018-05-11 17:30:46 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters_table.rb
+++ db/migrate/005_create_characters_table.rb
@@ -2,8 +2,7 @@ class CreateCharactersTable < ActiveRecord::Migration[4.2]
   def change
     create_table :characters do |t|
       t.string :name
-      t.integer :show_id
-
+      t.integer :show_id  
     end
   end
 end
