83ee0063e329b269032f1e8f4d5b55a4b5f7cc33
Rebecca Tong <rtong@buffalo.edu>
2018-05-11 17:36:17 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,10 +1,10 @@
 class Actor < ActiveRecord::Base
-  # 
-  # has_many :characters
-  # has_many :shows, through: :characters
-  #
-  # def full_name
-  #   [self.first_name, self.last_name].join(' ')
-  # end
+
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    [self.first_name, self.last_name].join(' ')
+  end
 
 end
